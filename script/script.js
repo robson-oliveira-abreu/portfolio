1dae5e8a474cc246ec483ef28568c0d1c891f0d4
Robson Oliveira <robsonsilva.abreu@outlook.com>
2022-03-26 19:30:05 -0300

    commit atualização estrutura finalizada

--- script/script.js
+++ script/script.js
@@ -1,36 +1,13 @@
 let prev = document.getElementById("prev")
 let next = document.getElementById("next")
-let slideIndex = 1;
-
-showSlides(slideIndex);
 
 prev.addEventListener("click", () => {
-    plusSlider(-1)
+
+    window.document.getElementById("scroll").scrollTo(-1000, 0)
 })
 
 next.addEventListener("click", () => {
-    plusSlider(1)
+
+    window.document.getElementById("scroll").scrollTo(1000,0)
 })
 
-function plusSlider(n) {
-    showSlides(slideIndex += n);
-}
-
-function prevFunction() {
-    showSlides(slideIndex += -1)
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("projects-page");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].classList.add("inactive-project-page");
-    }
-    slides[slideIndex-1].classList.remove("inactive-project-page");
-  }
