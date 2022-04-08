379d4a3228f7f5884ef46f9041f476bba33f1344
Robson Oliveira <robsonsilva.abreu@outlook.com>
2022-04-08 01:50:33 -0300

    update responsive and menu-button mobile

--- script/script.js
+++ script/script.js
@@ -1,21 +1,45 @@
 const scrollLinks = document.querySelectorAll(".scroll-to")
-const prev = document.getElementById("prev")
-const next = document.getElementById("next")
+const menuButton = document.querySelector(".section-menu")
+const mobileNavBar = document.querySelector(".mobileNavBar")
+const prev = document.querySelector("#prev")
+const next = document.querySelector("#next")
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle("on")
+
+    menuClassList = menuButton.getAttribute("class").slice(-2)
+
+    if (menuClassList == "on") {
+        mobileNavBar.classList.add("activeMenu")
+        body.classList.add("hiddenScrollBody") 
+
+    } else {
+        mobileNavBar.classList.remove("activeMenu")
+        body.classList.remove("hiddenScrollBody")
+    }
+
+})
 
 scrollLinks.forEach((item) => {
     item.addEventListener("click", () => {
+
+        mobileNavBar.classList.remove("activeMenu")
+        menuButton.classList.remove("on")
+        body.classList.remove("hiddenScrollBody")
+
         const el = document.getElementById(item.getAttribute("data-link"))
-        el.scrollIntoView({behavior: "smooth", block: "nearest"})
+        el.scrollIntoView({ behavior: "smooth", block: "nearest" })
+
     })
 })
 
 prev.addEventListener("click", () => {
 
-    window.document.getElementById("scroll").scrollTo(0, 0)
+    document.getElementById("scroll").scrollTo(0, 0)
 })
 
 next.addEventListener("click", () => {
 
-    window.document.getElementById("scroll").scrollBy(420,0)
+    document.getElementById("scroll").scrollBy(420, 0)
 })
 
